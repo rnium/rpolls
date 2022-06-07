cc3f7e0b6eb1dbd9d32bd9a54756349a3cf3f11e
rony <rniumo@gmail.com>
2022-06-08 00:43:32 +0600

    init

---
+++ polls/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Poll(models.Model):
+    title = models.CharField(max_length=250)
+    banner = models.ImageField(upload_to='polls/images/', default='polls/images/default.jpg')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='poll')
+    public = models.BooleanField(default=True)
+    active = models.BooleanField(default=True)
+    added = models.DateTimeField(auto_now_add=True)
+    views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class Choice(models.Model):
+    choicetext = models.CharField(max_length=100)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE, related_name='choice')
+
+    def __str__(self):
+        return f"choice of poll: {self.poll}"
+
+
+class Vote(models.Model):
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE, related_name='choicevote')
+    voter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='uservote')
+    vote_time = models.DateTimeField(auto_now_add=True)
+    vote_update_time = models.DateTimeField(auto_now=True)
+
